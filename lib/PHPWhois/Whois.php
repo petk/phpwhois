92e06a9058492fa6789ed6c6e99a7bcb7a5249ec
Peter Kokot <peterkokot@gmail.com>
2013-09-28 01:46:34 +0200

    version added

--- lib/PHPWhois/Whois.php
+++ lib/PHPWhois/Whois.php
@@ -1,2 +1,25 @@
 <?php
 
+/*
+ * This file is part of the PHPWhois
+ *
+ * (c) Peter Kokot <peterkokot@gmail.com>
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPWhois;
+
+/**
+ * Whois
+ * @author Peter Kokot <peterkokot@gmail.com>
+ */
+class Whois
+{
+    const VERSION = "1.0-dev";
+
+    public function __construct() {
+
+    }
+}

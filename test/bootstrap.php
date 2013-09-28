e6caf555917f0029ffb58c7153e7c7cc07472e96
Peter Kokot <peterkokot@gmail.com>
2013-09-28 17:23:33 +0200

    autoloader starting

---
+++ test/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of PHPWhois
+ *
+ *  (c) Peter Kokot <peterkokot@gmail.com>
+ *
+ *  For the full copyright and license information, please view the LICENSE
+ *  file that was distributed with this source code.
+ */
+
+require_once dirname(__FILE__).'/..lib/PHPWhois/Autoloader.php';
+
